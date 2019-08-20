56b2dc0fba260b258331951e60706122fdebdcec
xiexinfang <1036554353@qq.com>
2019-08-20 20:41:13 +0800

    10

--- js-workspace/main.js
+++ js-workspace/main.js
@@ -1,12 +1,48 @@
-function createMultiplyTable(start,end){
-    return table;
+
+
+function compare(start, end){
+    if(start < end){
+        return -1;
+    }else if(start == end){
+        return 0;
+    }else{
+        return 1;
+    }
 }
 
-function checkInput(start,end){
-    return isValid;
+function isRangeNumber(number){
+    if(number >=1 && number <=1000){
+        return true;
+    }
+    return false;
 }
 
-function createTable(start,end){
-    return table;
+function calculate(start, end){
+
+    var result = new Array();
+    for(let i = start; i < end + 1; i++){
+        let list = new Array();
+        for(let j = start; j <= i; j++){
+            let tmpString = j + "*" + i + "=" + i * j;
+            list.push(tmpString);
+        }
+        result.push(list);
+    }
+    return result;
 }
-module.exports = createMultiplyTable;
+
+function createMultilyTable(start, end){
+    if(compare(start, end) > 0){
+        return null
+    }
+    if(!isRangeNumber(start) || !isRangeNumber(end)){
+        return null;
+    }
+    var multilyTable = calculate(start, end);
+
+    return multilyTable;
+}
+
+
+
+module.exports = createMultilyTable;
